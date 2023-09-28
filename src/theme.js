323de1a572edc9864e3dde7954685f3268978923
Caroline <caroline.haoud@columbia.edu>
2023-09-27 21:58:29 -0400

    added theme.js and components folder

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    white: "#FFFFFF",
+    lightblue: "#9FB7BF",
+  },
+  sizes: {
+    mobile: "767px",
+    tablet: "992px",
+    aboveTablet: "993px",
+    aboveLargeTablet: "1024px",
+    desktop: "1200px",
+    largeDesktop: "1440px",
+  },
+};
+
+export default theme;
