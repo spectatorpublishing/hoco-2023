aa2a70091d38f3a20b9879742a415604971f0638
aer2221 <110146633+aer2221@users.noreply.github.com>
2023-10-01 13:19:57 -0400

    building out basic navigation bar

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function NavigationBar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/university">University</a></li>
+        <li><a href="/city-news">City News</a></li>
+        <li><a href="/sports">Sports</a></li>
+        <li><a href="/a-c">A&C</a></li>
+        <li><a href="/opinion">Opinion</a></li>
+        <li><a href="/spectrum">Spectrum</a></li>
+        <li><a href="/city-news">City News</a></li>
+
+      </ul>
+    </nav>
+  );
+}
+
+export default NavigationBar;
