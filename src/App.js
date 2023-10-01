d58e313b264aee27b7714f4f937f5e9451e86138
aer2221 <110146633+aer2221@users.noreply.github.com>
2023-10-01 14:04:49 -0400

    adding spec logo above navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar';
-import Film from './components/Film'
+import Film from './components/Film';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className="App">
+      <Logo></Logo>
       <NavigationBar></NavigationBar>
     </div>
   );
---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+import speclogo from '../speclogo.svg';
+
+function Logo() {
+    return (
+        <img src={speclogo} alt="logo" />
+    );
+  }
+  
+  export default Logo;
