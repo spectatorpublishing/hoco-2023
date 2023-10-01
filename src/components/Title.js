885c0574fd15ae23ab525367a8e849f9c824b93b
aer2221 <110146633+aer2221@users.noreply.github.com>
2023-10-01 12:43:36 -0400

    building out title

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <header>
+      <h1>Homecoming 2023</h1>
+      {/* Add navigation links, buttons, or any other content here */}
+    </header>
+  );
+}
+
+export default Title;
