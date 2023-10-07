b99fa0ab7341364b7149eb43e75cfa6267758e23
Gabriel Trigo <gabrielguerratrigo20@gmail.com>
2023-10-07 17:38:31 -0400

    credits done (I think?)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
+import Credits from './components/Credits'
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <Credits/>
     </div>
   );
 }
