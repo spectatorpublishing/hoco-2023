3d92c23383970673d59aa3c57de4bfd9ab9fa134
Gabriel Trigo <gabrielguerratrigo20@gmail.com>
2023-10-08 17:49:01 -0400

    Started lights component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
 import Credits from './components/Credits'
+import LightsComponent from './components/LightsComponent/LightsComponent'
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <LightsComponent/>
       <Credits/>
     </div>
   );
