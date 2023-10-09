b240ca631b8d0c8e0ca3bbd2424a5c3bf2c00158
Gabriel Trigo <gabrielguerratrigo20@gmail.com>
2023-10-08 22:09:23 -0400

    Started Lights component

--- src/components/LightsComponent/LightsComponent.js
+++ src/components/LightsComponent/LightsComponent.js
@@ -1,9 +1,31 @@
 import React from "react";
+import lights from './lights.png'
 import TapedPicture from './TapedPicture'
+import styled from 'styled-components';
+
+const ImgContainer = styled.img`
+    width: 100%;
+`
+
+const PicturesContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin-bottom: 2rem;
+`
 
 function LightsComponent() {
     return (
-        <TapedPicture/>
+        <>
+            <ImgContainer
+                src = { lights }
+            />
+            <PicturesContainer>
+                <TapedPicture title={ "Columbia Policies on Gender Neutral Bathrooms" } author={ "Ramel Nunez" }/>
+                <TapedPicture title={ "Columbia Policies on Gender Neutral Bathrooms" } author={ "hey" }/>
+                <TapedPicture title={ "hey" } author={ "hey" }/>
+            </PicturesContainer>
+        </>
     )
 }
 
