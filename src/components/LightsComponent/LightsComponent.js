2822cb5fd49cf09980099acf0cba981c969fc50e
Caroline Haoud <caroline.haoud@columbia.edu>
2023-10-09 19:04:47 -0400

    fixed middle photo to align closely with figma

--- src/components/LightsComponent/LightsComponent.js
+++ src/components/LightsComponent/LightsComponent.js
@@ -2,31 +2,30 @@ import React from "react";
 import lights from './lights.png'
 import TapedPicture from './TapedPicture'
 import styled from 'styled-components';
+import MidLights from "./MidLights";
 
 const ImgContainer = styled.img`
     width: 100%;
-`
+`;
 
 const PicturesContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
     margin-bottom: 2rem;
-`
+`;
 
 function LightsComponent() {
     return (
         <>
-            <ImgContainer
-                src = { lights }
-            />
+            <ImgContainer src={lights} alt="Description of lights image" />
             <PicturesContainer>
-                <TapedPicture title={ "Columbia Policies on Gender Neutral Bathrooms" } author={ "Ramel Nunez" }/>
-                <TapedPicture title={ "Columbia Policies on Gender Neutral Bathrooms" } author={ "hey" }/>
-                <TapedPicture title={ "hey" } author={ "hey" }/>
+                <TapedPicture title={"Columbia Policies on Gender Neutral Bathrooms"} author={"Ramel Nunez"} />
+                <MidLights title={"Columbia Policies on Gender Neutral Bathrooms"} author={"hey"} />
+                <TapedPicture title={"hey"} author={"hey"} />
             </PicturesContainer>
         </>
-    )
+    );
 }
 
 export default LightsComponent;
