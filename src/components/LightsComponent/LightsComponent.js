1d391a90181fec7652caf6087e98d0fe13d3b0c3
Gabriel Trigo <gabrielguerratrigo20@gmail.com>
2023-10-09 22:01:24 -0400

    lights component mobile version

--- src/components/LightsComponent/LightsComponent.js
+++ src/components/LightsComponent/LightsComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import lights from './lights.png'
 import TapedPicture from './TapedPicture'
 import styled from 'styled-components';
@@ -14,8 +14,33 @@ const PicturesContainer = styled.div`
     margin-bottom: 2rem;
 `
 
-function LightsComponent() {
-    return (
+function YourComponent() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Update window width when the component mounts or when window is resized
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Attach an event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    console.log(window.innerWidth);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [window.innerWidth]);
+
+  // Define the threshold width
+  const thresholdWidth = 900; // Adjust this value to your desired threshold
+
+  return (
+    <>
+      {windowWidth > thresholdWidth ? (
+        // Content to render when display width is above the threshold
         <>
             <ImgContainer
                 src = { lights }
@@ -26,7 +51,25 @@ function LightsComponent() {
                 <TapedPicture title={ "hey" } author={ "hey" }/>
             </PicturesContainer>
         </>
+<<<<<<< Updated upstream
     )
 }
 
 export default LightsComponent;
+=======
+      ) : (
+        // Content to render when display width is below or equal to the threshold
+        <>
+            <ImgContainer src={lights} alt="Description of lights image" />
+            <PicturesContainer>
+                <TapedPicture title={"Columbia Policies on Gender Neutral Bathrooms"} author={"Ramel Nunez"} />
+                <TapedPicture title={"hey"} author={"hey"} />
+            </PicturesContainer>
+        </>
+      )}
+    </>
+  );
+}
+
+export default YourComponent;
+>>>>>>> Stashed changes
